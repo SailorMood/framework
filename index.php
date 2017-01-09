a7e0c22242a364baf68d9af6e503226f76561d99
Dell <bastienthefly@live.fr>
2017-01-09 10:04:11 +0100

    Yeah

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dell
- * Date: 09/01/2017
- * Time: 09:36
- */
+class Model
+{
+    public $string;
+
+    public function __construct()
+    {
+        $this -> string = "Yeah le MVC !";
+    }
+}
+
+class View
+{
+    private $model;
+    private $controller;
+
+    public function __construct($controller, $model)
+    {
+        $this -> controller = $controller;
+        $this -> model = $model;
+    }
+
+    public function output()
+    {
+        return "<p>". $this -> model -> string. "</p>";
+    }
+}
+
+class Controller
+{
+    private $model;
+
+    public function __construct($model)
+    {
+        $this -> model = $model;
+    }
+}
+
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+echo $view -> output();
